6ac373e88d4d0482ebf79dc19f611ee01b5f964a
stu18 <susforstu18@gmail.com>
2020-05-11 07:00:18 +0000

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,6 +22,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "password" } }
     end
     follow_redirect!
+    assert is_logged_in?
     assert_template 'users/show'
   end
 end
